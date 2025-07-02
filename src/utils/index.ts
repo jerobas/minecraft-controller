1ee93b2c4250df36ce04c109dc7983655142fba8
flaviozno <flavio.vezono@ufu.br>
2025-07-02 16:28:14 -0300

    ft

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,14 +5,18 @@ export const Init = () => {
   const serverDir = path.resolve("./minecraft_server");
   const pluginsDir = path.join(serverDir, "plugins");
   const eulaFile = path.join(serverDir, "eula.txt");
-  const serverPropertiesFile = path.join(serverDir, "server.properties");
   const templateFile = path.resolve("src/template/server.properties");
+  const templateImage = path.resolve("src/template/server-icon.png");
+  const serverTemplateImage = path.join(serverDir, "server-icon.png");
+  const serverPropertiesFile = path.join(serverDir, "server.properties");
 
   if (!fs.existsSync(serverDir)) fs.mkdirSync(serverDir, { recursive: true });
   if (!fs.existsSync(pluginsDir)) fs.mkdirSync(pluginsDir, { recursive: true });
   if (!fs.existsSync(eulaFile)) fs.writeFileSync(eulaFile, "eula=true\n");
   if (!fs.existsSync(serverPropertiesFile))
     fs.copyFileSync(templateFile, serverPropertiesFile);
+  if (!fs.existsSync(serverTemplateImage))
+    fs.copyFileSync(templateImage, serverTemplateImage);
 
   return { serverDir, pluginsDir, serverPropertiesFile };
 };
